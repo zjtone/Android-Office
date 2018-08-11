40c15404117945a5460698858a856ecd23494c13
zjtone <13682427795@163.com>
2018-08-11 10:16:22 +0800

    添加使用示例

--- app/src/main/java/com/zjtone/androidoffice/MainActivity.java
+++ app/src/main/java/com/zjtone/androidoffice/MainActivity.java
@@ -1,7 +1,12 @@
 package com.zjtone.androidoffice;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.wxiwei.office.IOffice;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        IOffice iOffice = new IOffice() {
+            @Override
+            public Activity getActivity() {
+                return MainActivity.this;
+            }
+
+            @Override
+            public String getAppName() {
+                return "ioffice";
+            }
+
+            @Override
+            public File getTemporaryDirectory() {
+                return null;
+            }
+
+            @Override
+            public void openFileFinish() {
+            }
+        };
     }
 }
